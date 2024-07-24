aaca0fde7f255ec4b3ff9626b5b5ca3875b8aea4
davinraff <davinraffilio9@gmail.com>
2024-07-24 09:05:34 +0700

    test

--- app/src/main/java/com/example/ux_lab/Home.java
+++ app/src/main/java/com/example/ux_lab/Home.java
@@ -1,4 +1,9 @@
 package com.example.ux_lab;
 
 public class Home {
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
