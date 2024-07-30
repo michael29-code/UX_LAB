0fde1f9b9b6d988db0fe64058fdc025eaa83e28c
davinraff <davinraffilio9@gmail.com>
2024-07-30 15:20:50 +0700

    davin don

--- app/src/main/java/com/example/ux_lab/ContactUsActivity.java
+++ app/src/main/java/com/example/ux_lab/ContactUsActivity.java
@@ -6,18 +6,19 @@ import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AboutUsActivity extends AppCompatActivity {
+public class ContactUsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about_us);
+        setContentView(R.layout.contact_us);
 
-        Button btnContactUs = findViewById(R.id.btnContactUs);
-        btnContactUs.setOnClickListener(new View.OnClickListener() {
+        // Find the "About Us" button and set an OnClickListener
+        Button btnAboutUs = findViewById(R.id.btnAboutUs);
+        btnAboutUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Start the Contact Us activity
-                Intent intent = new Intent(AboutUsActivity.this, ContactUsActivity.class);
+                // Start the About Us activity
+                Intent intent = new Intent(ContactUsActivity.this, AboutUsActivity.class);
                 startActivity(intent);
             }
         });
